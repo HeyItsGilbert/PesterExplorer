6e661e2fb9739595fde78ba79a8fd846afb14f74
Gilbert Sanchez <gilbsanchez@tesla.com>
2025-05-28 07:53:58 -0700

    feat(Get-ShortcutKeyPanel): ✨ add detailed comment-based help for function
    - Enhanced the function documentation with a comprehensive `.SYNOPSIS`, `.DESCRIPTION`, and `.EXAMPLE`.
    - Improved readability and usability of the shortcut keys panel for the Pester Explorer TUI.

--- PesterExplorer/Private/Get-ShortcutKeyPanel.ps1
+++ PesterExplorer/Private/Get-ShortcutKeyPanel.ps1
@@ -1,4 +1,23 @@
 function Get-ShortcutKeyPanel {
+    <#
+    .SYNOPSIS
+    Get a panel displaying shortcut keys for the Pester Explorer TUI.
+
+    .DESCRIPTION
+    This function generates a panel that displays the shortcut keys available
+    in the Pester Explorer TUI. The keys are formatted for display using
+    Spectre.Console, providing a user-friendly interface for navigating the
+    Pester Explorer. The panel includes common shortcuts for navigation,
+    exploration, and exiting the TUI. It returns a formatted panel that can be
+    displayed in the Pester Explorer interface.
+
+    .EXAMPLE
+    $shortcutPanel = Get-ShortcutKeyPanel
+
+    This example retrieves a panel displaying the shortcut keys for the Pester
+    Explorer TUI. The panel includes keys for navigation, exploration, and
+    exiting the TUI, formatted for easy readability.
+    #>
     [CmdletBinding()]
     $shortcutKeys = @(
         "Up, Down - Navigate",
@@ -7,10 +26,14 @@ function Get-ShortcutKeyPanel {
         "Ctrl+C - Exit"
         # TODO: Add a key to jump to the test
     )
+    $formatSpectreAlignedSplat = @{
+        HorizontalAlignment = 'Center'
+        VerticalAlignment = 'Middle'
+    }
     $result = $shortcutKeys | Foreach-Object {
         "[grey]$($_)[/]"
     } | Format-SpectreColumns -Padding 5 |
-        Format-SpectreAligned -HorizontalAlignment Center -VerticalAlignment Middle |
+        Format-SpectreAligned @formatSpectreAlignedSplat |
         Format-SpectrePanel -Expand -Border 'None'
     return $result
 }
